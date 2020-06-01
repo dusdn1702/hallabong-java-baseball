580c4f3f06d1e11020fa207337e79c232ea6e903
dusdn1702 <dusdn1702@naver.com>
2020-06-01 16:54:46 +0900

    정리

---
+++ src/main/java/com/baseball/yeonwoo/Computer.java
@@ -0,0 +1,35 @@
+/*
+ * play.java            1.0.0       2020-06-01
+ *
+ * Copyright (c) 2020 Yeonwoo Cho
+ * ComputerScience, ProgrammingLanguage, Java, Seoul, KOREA
+ * All rights reserved
+ */
+
+package com.baseball.yeonwoo;
+
+/**
+ * play.class
+ * 게임을 진행하는 클래스
+ *
+ * @version     1.0     2020년 6월 1일
+ * @author      조연우
+ */
+
+public class play {
+    public static void gameStart(){
+
+    }
+    public static void makeGame(){
+
+    }
+    public static void printHint(){
+
+    }
+    public static void isRight(){
+
+    }
+    public static void againOrDone(){
+
+    }
+}
---
+++ src/main/java/com/baseball/yeonwoo/Player.java
@@ -0,0 +1,4 @@
+package com.baseball.yeonwoo;
+
+public class player {
+}
