0b22745b2284bf1bc7ca36a2f89fb9211e085616
dusdn1702 <dusdn1702@naver.com>
2020-06-07 00:13:57 +0900

    변수 명명 수정, 매직넘버 생성
    변수 이름을 최대한 축약하지 않고 변수가 의미하는 바를 이름을 보고 이해할 수 있도록 하였다.
    숫자의 길이가 3인 경우를 다루고 있으므로, 이 때 사용했던 3을 모두 NUMBER_LENGTH라는 매직 넘버로 바꾸었다.

--- src/main/java/com/baseball/yeonwoo/Computer.java
+++ src/main/java/com/baseball/yeonwoo/Computer.java
@@ -16,101 +16,101 @@ import java.util.Scanner;
  * Computer.class
  * 게임을 진행하는 클래스
  *
- * @version     2.0     2020년 6월 6일
- * @author      조연우
+ * @author 조연우
+ * @version 2.0     2020년 6월 6일
  */
 
 public class Computer {
-    ArrayList<Character> answer = new ArrayList<Character>();     //컴퓨터가 만든 답
-    ArrayList<Character> input = new ArrayList<Character>();      //게임하는 사람이 추측한 입력 값
+    public static final int NUMBER_LENGTH = 3;
 
     public Player newPlayer;
 
-    protected void gameStart(){    //처음 시작
+    ArrayList<Character> computerAnswer = new ArrayList<Character>();     //컴퓨터가 만든 답
+    ArrayList<Character> playerInput = new ArrayList<Character>();      //게임하는 사람이 추측한 입력 값
+
+    protected void gameStart() {    //처음 시작
         makeAnswer();
     }
 
-    private void makeAnswer(){
+    private void makeAnswer() {
+        String currentNumber = "";
         Random random = new Random();
-        String nowNum = "";
-        for(int i=0; i<3; i++){
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
             String number = Integer.toString(random.nextInt(10));
-            if(!number.equals(0)){
-                if(!nowNum.contains(number)){
-                    nowNum += number;
-                } else{
+            if (!number.equals(0)) {
+                if (!currentNumber.contains(number)) {
+                    currentNumber += number;
+                } else {
                     i--;
                 }
-            } else{
+            } else {
                 i--;
             }
         }
-        for(int i=0; i<nowNum.length(); i++) {
-            answer.add(nowNum.charAt(i));
+        for (int i = 0; i < currentNumber.length(); i++) {
+            computerAnswer.add(currentNumber.charAt(i));
         }
         newGame();
     }
 
-    private void newGame(){  //새롭게 입력받는 부분
+    private void newGame() {  //새롭게 입력받는 부분
         newPlayer = new Player();
-
-        input = newPlayer.inputAnswer();
+        playerInput = newPlayer.inputAnswer();
         printHint();
     }
 
-    private void printHint(){    //입력에 대한 힌트 계산, 출력
+    private void printHint() {    //입력에 대한 힌트 계산, 출력
         int strike = 0;
         int ball = 0;
-
-        for(int i=0; i<3; i++) {
-            if(answer.get(i)==input.get(i)){
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            if (computerAnswer.get(i) == playerInput.get(i)) {
                 strike++;
             } else {
-                ball+=ballCount(i);
+                ball += ballCount(i);
             }
         }
-        if(strike == 3){
+        if (strike == 3) {
             isRight();
-        }else if((ball == 0) && (strike == 0)) {
+        } else if ((ball == 0) && (strike == 0)) {
             System.out.println("nothing");
             newGame();
-        }else if(strike == 0){
-            System.out.println(ball+"ball");
+        } else if (strike == 0) {
+            System.out.println(ball + "ball");
             newGame();
-        }else if(ball == 0){
-            System.out.println(strike+"strike");
+        } else if (ball == 0) {
+            System.out.println(strike + "strike");
             newGame();
-        }else{
-            System.out.println(strike+"strike "+ball+"ball");
+        } else {
+            System.out.println(strike + "strike " + ball + "ball");
             newGame();
         }
     }
 
-    private int ballCount(int i){
+    private int ballCount(int i) {
         int b = 0;
-        for(int j=0; j<3; j++){
-            if(answer.get(i)==input.get(j)){
+        for (int j = 0; j < NUMBER_LENGTH; j++) {
+            if (computerAnswer.get(i) == playerInput.get(j)) {
                 b++;
             }
         }
         return b;
     }
 
-    private static void isRight(){
+    private static void isRight() {
         System.out.println("You Win! Game Finish");
         System.out.print("if you want restart-> 1 done -> 2 : ");
         againOrDone();
     }
 
-    private static void againOrDone(){
+    private static void againOrDone() {
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
-        if(num == 1){
+        if (num == 1) {
             Computer newComputer = new Computer();
             newComputer.gameStart();
-        } else if(num == 2){
+        } else if (num == 2) {
             System.exit(0);
-        } else{
+        } else {
             System.out.print("wrong number. input again : ");
             againOrDone();
         }
--- src/main/java/com/baseball/yeonwoo/Player.java
+++ src/main/java/com/baseball/yeonwoo/Player.java
@@ -15,51 +15,53 @@ import java.util.Scanner;
  * Player.class
  * 게임을 하는 클래스
  *
- * @version     2.0     2020년 6월 6일
- * @author      조연우
+ * @author 조연우
+ * @version 2.0     2020년 6월 6일
  */
 
 public class Player {
-    Scanner scan = new Scanner(System.in);
+    public static final int NUMBER_LENGTH = 3;
     String myAnswer = "";
-    ArrayList<Character> playerAnswer = new ArrayList<Character>();
 
-    protected ArrayList<Character> inputAnswer(){
+    ArrayList<Character> playerAnswer = new ArrayList<Character>();
+    Scanner scan = new Scanner(System.in);
+
+    protected ArrayList<Character> inputAnswer() {
         System.out.print("input three numbers : ");
         myAnswer = scan.nextLine();
-        if(!rangeOk(myAnswer) || (!lengthOk(myAnswer)) || (!noSame(myAnswer))) {
+        if (!rangeOk(myAnswer) || (!lengthOk(myAnswer)) || (!noSame(myAnswer))) {
             somethingWrong();
         }
-        for(int i=0; i<myAnswer.length(); i++) {
+        for (int i = 0; i < myAnswer.length(); i++) {
             playerAnswer.add(myAnswer.charAt(i));
         }
         return playerAnswer;
     }
 
-    private boolean rangeOk(String s){
+    private boolean rangeOk(String s) {
         char[] c = s.toCharArray();
 
-        for(int i=0; i<3; i++){
-            if((c[i]<'1') || (c[i]>'9')){
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            if ((c[i] < '1') || (c[i] > '9')) {
                 return false;
             }
         }
         return true;
     }
 
-    private boolean lengthOk(String s){
-        if(s.length() != 3) {
+    private boolean lengthOk(String s) {
+        if (s.length() != NUMBER_LENGTH) {
             return false;
         }
         return true;
     }
 
-    private boolean noSame(String s){
+    private boolean noSame(String s) {
         char[] in = s.toCharArray();
 
-        for(int i=0; i<s.length()-1; i++){
-            for(int j=i+1; j<s.length(); j++){
-                if(in[i] == in[j]){
+        for (int i = 0; i < s.length() - 1; i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (in[i] == in[j]) {
                     return false;
                 }
             }
@@ -67,7 +69,7 @@ public class Player {
         return true;
     }
 
-    private void somethingWrong(){
+    private void somethingWrong() {
         System.out.println("input again");
         scan = new Scanner(System.in);
         inputAnswer();
